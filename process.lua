0d21166c5e46e99dc52aafdb32d7c5bdc6f80b73
Michael Reichert <michael.reichert@geofabrik.de>
2026-09-21 15:57:21 +0200

    Minor improvements to #44
    * Drop support of "label" roles for place=island on relations because
      most of them are multipolygons, not boundaries.
    * Write 0 as default population.

--- process.lua
+++ process.lua
@@ -1154,7 +1154,10 @@ function way_function()
 	-- island mames into places layer
 	-- because not all have name as point
 	if place == "island" then
-		LayerAsCentroid("place_labels", "polylabel", "label")
+		-- We don't use relation members with role "label" because almost all place=island
+		-- are relations with type=multipolygon. Nodes are not permitted as members in these
+		-- relations.
+		LayerAsCentroid("place_labels", "polylabel")
 		Attribute("kind", place)
 		AttributeNumeric("area", area)
 		MinZoom(8)
@@ -1162,7 +1165,7 @@ function way_function()
 		local population = Find("population")
 		local populationNum = tonumber(population)
 		if populationNum ~= nil then
-			AttributeNumeric("population", populationNum)
+			AttributeNumeric("population", populationNum or 0)
 			ZOrder(populationNum)
 		end
 	end
